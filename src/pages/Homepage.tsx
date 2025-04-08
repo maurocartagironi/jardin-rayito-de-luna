3d4ca13cbf78c3a3b3a0a6dfe68847c4c9367980
Mauro Cartagironi <cartagironi.mauro@gmail.com>
2025-04-08 16:28:28 -0300

    Labels

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,7 +2,7 @@ import { Nosotros } from '@components/home/Nosotros';
 import { Hero } from '@components/home/Hero';
 import { Pilares } from '@/components/home/Pilares';
 import { Actividades } from '@components/home/Actividades';
-import { Testimonios } from '@components/home/Testimonios';
+import { Testimonios } from '@/components/home/Testimonios';
 import { Galeria } from '@components/home/Galeria';
 import { Contacto } from '@components/home/Contacto';
 import Noticias from '../components/home/Noticias';
